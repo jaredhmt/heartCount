114a5559ce14afedcbe4361a5d9203d5f5a9b17b
Jared Hale <jaredhmt@gmail.com>
2021-03-01 00:02:13 -0700

    update info.txt

--- heartCount/ContentView.swift
+++ heartCount/ContentView.swift
@@ -24,7 +24,12 @@ func requestAccess() -> Void {
         }
     }
 }
-func fetchHealthData() -> Void {
+func fetchHealthData() -> (bpms: [Double], times: [Date]) {
+    
+    var bpms: [Double] = []
+    var times: [Date] = []
+    
+    
     let healthStore = HKHealthStore()
     if HKHealthStore.isHealthDataAvailable() {
         let readData = Set([
@@ -42,18 +47,19 @@ func fetchHealthData() -> Void {
                 anchorComponents.day! -= offset
                 anchorComponents.hour = 0
                 
-                guard let anchorDate = Calendar.current.date(from: anchorComponents) else {
-                    fatalError("*** unable to create a valid date from the given components ***")
-                }
+//                guard let anchorDate = Calendar.current.date(from: anchorComponents) else {
+//                    fatalError("*** unable to create a valid date from the given components ***")
+//                }
                 
                 let interval = NSDateComponents()
                 interval.minute = 1
                                     
                 let endDate = Date()
                                             
-                guard let startDate = calendar.date(byAdding: .month, value: -1, to: endDate) else {
-                    fatalError("*** Unable to calculate the start date ***")
-                }
+//                guard let startDate = calendar.date(byAdding: .month, value: -1, to: endDate) else {
+//                    fatalError("*** Unable to calculate the start date ***")
+//                }
+                let startDate = Calendar.current.date(bySettingHour: 0, minute: 0, second: 0, of: Date())!
                                     
                 guard let quantityType = HKObjectType.quantityType(forIdentifier: HKQuantityTypeIdentifier.heartRate) else {
                     fatalError("*** Unable to create a step count type ***")
@@ -62,7 +68,7 @@ func fetchHealthData() -> Void {
                 let query = HKStatisticsCollectionQuery(quantityType: quantityType,
                                                         quantitySamplePredicate: nil,
                                                             options: .discreteAverage,
-                                                            anchorDate: anchorDate,
+                                                            anchorDate: startDate,
                                                             intervalComponents: interval as DateComponents)
                 
                 query.initialResultsHandler = {
@@ -78,9 +84,12 @@ func fetchHealthData() -> Void {
                             let date = statistics.startDate
                             //for: E.g. for steps it's HKUnit.count()
                             let value = quantity.doubleValue(for: HKUnit(from: "count/min"))
-                            print("done")
+//                            print("done")
                             print(value)
-                            print(date)
+//                            print(date)
+                            bpms.append(value)
+                            times.append(date)
+                            
                                                         
                         }
                     }
@@ -88,21 +97,43 @@ func fetchHealthData() -> Void {
                 }
                 
                 healthStore.execute(query)
+//                return(bpms, times)
+                
                 
             } else {
                 print("Authorization failed")
- 
             }
         }
     }
+    return(bpms, times)
 }
+
+func calculatebpms(_ bpms:[Double], _ times:[Date]) -> Void {
+    print("HELLO I AM CALCULATING")
+    var absTime: [Double] = []
+    let calendar = Calendar.current
+    for time in times{
+//        print(time)
+        var hour = calendar.component(.hour, from: time)
+        var min = calendar.component(.minute, from: time)
+        var sec = calendar.component(.second, from: time)
+        var thisAbsTime = Double(hour) * 60.0 + Double(min) + Double(sec) / 60.0
+//        print(thisAbsTime)
+        absTime.append(thisAbsTime)
+    }
+        
+    print("I AM CALCULATED")
+}
+
 var healthStore : HKHealthStore?
 
 struct ContentView: View {
     var body: some View {
         if HKHealthStore.isHealthDataAvailable() {
             mainView().onAppear(){
-                fetchHealthData()
+                var myData = fetchHealthData()
+                print(myData.bpms)
+                calculatebpms(myData.bpms,myData.times)
             }
             
         } else{
